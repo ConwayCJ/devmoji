bf54c9dc70c9e228bd687eff20b960941a58fafa
Travis Barnette <barnette.travis31@gmail.com>
2023-06-27 16:18:12 -0400

    rectified the cofee issue'

--- src/assets/Data.tsx
+++ src/assets/Data.tsx
@@ -1,34 +1,42 @@
-import { HourglassBottom, HouseSharp, WbSunny, MonitorHeart, CoffeeIcon } from '@mui/icons-material'
-import { PromptData } from '../main'
+import {
+  HourglassBottom,
+  HouseSharp,
+  WbSunny,
+  MonitorHeart,
+  Coffee,
+} from "@mui/icons-material";
+import { PromptData } from "../main";
 
 /**
  * Todo: Add credits, option for LinkedIn/GitHub/Socials links to contributor
- * 
+ *
  */
 
+const data: PromptData[] = [
+  {
+    prompt: [<WbSunny />, "day"],
+    answer: "sunny day",
+    socials: {
+      LIurl: "https://www.linkedin.com/in/conwaycj/",
+      GHurl: "https://github.com/ConwayCJ",
+    },
+  },
+  {
+    prompt: ["Out", "of", <HourglassBottom />],
+    answer: "out of time",
+  },
+  {
+    prompt: [<HouseSharp />, "party"],
+    answer: "house party",
+  },
+  {
+    prompt: ["Queen", "of", <MonitorHeart />, "'s"],
+    answer: "queen of hearts",
+  },
+  {
+    prompt: [<Coffee />, "is", "life"],
+    answer: "coffee is life",
+  },
+];
 
-const data: PromptData[] = [{
-  prompt: [<WbSunny />, "day"],
-  answer: "sunny day",
-  socials: {
-    LIurl: 'https://www.linkedin.com/in/conwaycj/',
-    GHurl: 'https://github.com/ConwayCJ',
-  }
-}, {
-  prompt: ["Out", "of", <HourglassBottom />],
-  answer: "out of time"
-}, {
-  prompt: [<HouseSharp />, "party"],
-  answer: "house party"
-}, {
-  prompt: ["Queen", "of", <MonitorHeart />, "'s"],
-  answer: 'queen of hearts'
-},
-{
-  prompt: [ <CoffeeIcon />, "is", "life"],
-  answer: 'coffee is life'
-}
-
-]
-
-export default data
+export default data;
