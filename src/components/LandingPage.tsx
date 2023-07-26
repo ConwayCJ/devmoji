e0ff82f89362656c0888690b5b35e9966bea4f75
ConwayCJ <conwaycj@plu.edu>
2023-07-26 01:17:06 -0700

    add form boilerplate

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import styles from '../styles/LandingPage.module.css'
 import { Link } from 'react-router-dom'
 import Footer from './Footer'
+import { FormEvent, FormEventHandler } from 'react'
+import axios from 'axios'
+
 
 export default function LandingPage() {
+
+
+  // const handlePost = () => {
+  //   const handlePost = () => {
+  //     axios.post('/api/data', postData)
+  //       .then((response) => {
+  //         console.log('Data successfully saved:', response.data);
+  //       })
+  //       .catch((error) => {
+  //         console.error('Error saving data:', error);
+  //       });
+  //   };
+  // }
+
+  const handleNewMessage: FormEventHandler = (e: FormEvent) => {
+
+  }
   return (
     <div className='w-full h-screen flex flex-col justify-between items-center'>
-      <div></div>
 
+      <div></div>
       <div>
-        <h1>Welcome to _appName_</h1>
+        <h1>Welcome to DevMoji</h1>
         <section>
           <p>Instruction: Guess the phrase</p>
           <p>For Devs: consider contributing on <a href="https://github.com/ConwayCJ/opensourceproject" target="_blank">GitHub</a></p>
@@ -19,6 +37,16 @@ export default function LandingPage() {
       <div>
         <Link className=' text-4xl uppercase border border-cyan-400 ring-2 shadow-lg p-1 rounded-xl' to={'/1'}>Get started</Link>
       </div>
+
+      {/* 
+      <form onSubmit={handleNewMessage} className='flex flex-col'>
+        <label>Leave your mark~</label>
+        <span>
+          <input></input>
+          <button>✔</button>
+        </span>
+      </form>
+       */}
 
 
       <Footer />
