655f85954e1514ed6d15cfbb4be64fa548f2ad22
Patrick Murray <patrick.murrayjr@gmail.com>
2023-08-13 17:04:43 -0400

    updated to 2 prompts;back to sleep; house hunting;

--- src/assets/Data.tsx
+++ src/assets/Data.tsx
@@ -1,70 +1,56 @@
 //Material UI Imports: https://mui.com/material-ui/material-icons/
-import { Coffee, AirplanemodeActive, Favorite } from '@mui/icons-material'
+import { Coffee, AirplanemodeActive, Favorite } from '@mui/icons-material';
 //FA React Icons Imports: https://react-icons.github.io/react-icons
-import { FaStar, FaChessQueen, FaBed, FaStepBackward, FaHouseUser, FaSmile, FaClock } from 'react-icons/fa';
+import { FaStar, FaChessQueen, FaBed, FaStepBackward, FaHouseUser } from 'react-icons/fa';
 //GI React Icons Imports: https://react-icons.github.io/react-icons
-import { GiBee, GiBowlingStrike, GiBowman, GiConversation, GiSewingNeedle, GiTireTracks } from 'react-icons/gi'
-import { PromptData } from '../main'
+import { GiBowman, GiTireTracks } from 'react-icons/gi';
+import { PromptData } from '../main';
 
-const data: PromptData[] = [{
-  prompt: [<FaStar />, <GiTireTracks />],
-  answer: "star trek",
-  socials: {
-    LIurl: 'https://www.linkedin.com/in/conwaycj/',
-    GHurl: 'https://github.com/ConwayCJ',
-  }
-}, {
-  prompt: [<Coffee />, "is", "life"],
-  answer: 'coffee is life'
-}, {
-  prompt: [<AirplanemodeActive />, "mode"],
-  answer: "airplane mode",
-  socials: {
-    LIurl: "https://www.linkedin.com/in/nereidarondon/",
-    GHurl: "https://github.com/NereidaRondon"
-  }
-}, {
-  prompt: [<FaChessQueen/>, "of", <Favorite/>],
-  answer: "queen of hearts",
-  socials: {
-    LIurl: "https://www.linkedin.com/in/rachelkvanhorn/",
-    GHurl: "https://github.com/RKVanHorn"
-  }
-}, {
-  prompt: [<GiSewingNeedle/>, <GiBee/>, "it"],
-  answer: "so be it",
-  socials: {
-    LIurl: "https://www.linkedin.com/in/patrick-f-murray-jr/",
-    GHurl: "https://github.com/patrick-murrayjr",
-  }
-}, {
-  prompt: [<GiBowlingStrike/>, "a", <GiConversation/>],
-  answer: "strike a conversation",
-  socials: {
-    LIurl: "https://www.linkedin.com/in/patrick-f-murray-jr/",
-    GHurl: "https://github.com/patrick-murrayjr",
-  }
-}, {
-  prompt: [<FaStepBackward/>, "to", <FaBed/>],
-  answer: "back to sleep",
-  socials: {
-    LIurl: "https://www.linkedin.com/in/patrick-f-murray-jr/",
-    GHurl: "https://github.com/patrick-murrayjr",
-  }
-}, {
-  prompt: [<FaHouseUser/>, <GiBowman/>, "ing"],
-  answer: "house hunting",
-  socials: {
-    LIurl: "https://www.linkedin.com/in/patrick-f-murray-jr/",
-    GHurl: "https://github.com/patrick-murrayjr",
-  }
-}, {
-  prompt: [<FaSmile/>, <FaClock/>],
-  answer: "happy hour",
-  socials: {
-    LIurl: "https://www.linkedin.com/in/patrick-f-murray-jr/",
-    GHurl: "https://github.com/patrick-murrayjr",
-  }
-}]
+const data: PromptData[] = [
+   {
+      prompt: [<FaStar />, <GiTireTracks />],
+      answer: 'star trek',
+      socials: {
+         LIurl: 'https://www.linkedin.com/in/conwaycj/',
+         GHurl: 'https://github.com/ConwayCJ',
+      },
+   },
+   {
+      prompt: [<Coffee />, 'is', 'life'],
+      answer: 'coffee is life',
+   },
+   {
+      prompt: [<AirplanemodeActive />, 'mode'],
+      answer: 'airplane mode',
+      socials: {
+         LIurl: 'https://www.linkedin.com/in/nereidarondon/',
+         GHurl: 'https://github.com/NereidaRondon',
+      },
+   },
+   {
+      prompt: [<FaChessQueen />, 'of', <Favorite />],
+      answer: 'queen of hearts',
+      socials: {
+         LIurl: 'https://www.linkedin.com/in/rachelkvanhorn/',
+         GHurl: 'https://github.com/RKVanHorn',
+      },
+   },
+   {
+      prompt: [<FaStepBackward />, 'to', <FaBed />],
+      answer: 'back to sleep',
+      socials: {
+         LIurl: 'https://www.linkedin.com/in/patrick-f-murray-jr/',
+         GHurl: 'https://github.com/patrick-murrayjr',
+      },
+   },
+   {
+      prompt: [<FaHouseUser />, <GiBowman />, 'ing'],
+      answer: 'house hunting',
+      socials: {
+         LIurl: 'https://www.linkedin.com/in/patrick-f-murray-jr/',
+         GHurl: 'https://github.com/patrick-murrayjr',
+      },
+   },
+];
 
-export default data
+export default data;
