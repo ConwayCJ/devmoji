5615b6c95aeae5864baca28bb424883f698f0ffa
Cmendence <chris.mendence@gmail.com>
2023-09-18 15:41:03 -0400

    Add prompt pool party

--- src/assets/Data.tsx
+++ src/assets/Data.tsx
@@ -1,11 +1,10 @@
 //Material UI Imports: https://mui.com/material-ui/material-icons/
-import { Coffee, AirplanemodeActive, Favorite, AllInclusive, Loop, RocketLaunch, Construction } from '@mui/icons-material'
+import { Coffee, AirplanemodeActive, Favorite, AllInclusive, Loop, RocketLaunch, Construction, Celebration, Pool } from '@mui/icons-material'
 //FA React Icons Imports: https://react-icons.github.io/react-icons
 import { FaStar, FaChessQueen, FaSpider, FaStepBackward, FaHouseUser, FaBed, FaPaintRoller } from 'react-icons/fa'
 //GI React Icons Imports: https://react-icons.github.io/react-icons
 import { GiTireTracks, GiBowman, GiBearFace } from 'react-icons/gi'
-import CelebrationIcon from '@mui/icons-material/Celebration';
-import PoolIcon from '@mui/icons-material/Pool';
+
 
 import { PromptData } from '../main'
 
@@ -86,7 +85,7 @@ const data: PromptData[] = [{
   }
 },
 {
-   prompt: [<PoolIcon />, <CelebrationIcon /> ],
+   prompt: [<Pool />, <Celebration /> ],
    answer: "pool party",
    socials: {
      LIurl: "https://www.linkedin.com/in/christopher-mendence-579245264/",
